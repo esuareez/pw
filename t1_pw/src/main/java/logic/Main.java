97d036fd66302e92c698877df180286bf658cef3
esuarez <exps0001@ce.pucmm.edu.do>
2022-05-29 11:01:39 -0400

    Item F

--- t1_pw/src/main/java/logic/Main.java
+++ t1_pw/src/main/java/logic/Main.java
@@ -1,22 +1,24 @@
 package logic;
 
 import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import io.javalin.Javalin;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        /*InputStream stream = System.in;
+        InputStream stream = System.in;
         Scanner scanner = new Scanner(stream);
+        System.out.println("Escriba la URL: ");
         String url = scanner.next();
-        scanner.close();*/
+        scanner.close();
 
-        Document doc = Jsoup.connect("https://www.maltamorena.com.do/registro-usuario").get();
+        Document doc = Jsoup.connect(url).get();
 
         /*
         Item A: Indicar la cantidad de lineas del recurso retornado
@@ -28,10 +30,11 @@ public class Main {
             cantLines++;
             line.getAllElements().next();
         }
-        System.out.println("Cantidad de lineas que hay en el HTML: "+cantLines);
+
+        System.out.println("\nCantidad de lineas que hay en el HTML: "+cantLines);
 
         /*
-        Item B:
+        Item B: Indicar la cantidad de párrafos (p) que contiene el documento HTML.
          */
         Elements p = doc.getElementsByTag("p");
         int cantP = 0; // Cantidad de <p> en el HTML
@@ -41,6 +44,10 @@ public class Main {
         }
         System.out.println("Cantidad de <p> que hay en el HTML: "+cantP);
 
+        /*
+        Item C: Indicar la cantidad de imágenes (img) dentro de los párrafos que
+                contiene el archivo HTML.
+         */
         Elements img = doc.getElementsByTag("img");
         int cantImg = 0; // Cantidad de <img> en el HTML
         for(Element im : img) {
@@ -48,7 +55,16 @@ public class Main {
             im.getAllElements().next();
         }
         System.out.println("Cantidad de <img> que hay en el HTML: "+cantImg);
+        /*
+        Item D y F: (D) -> Indicar la cantidad de formularios (form) que contiene el HTML por
+                    categorizando por el método implementado POST o GET.
+                    (F) -> Para cada formulario 'parseado', identificar que el método de envío
+                            del formulario sea POST y enviar una petición al servidor con el
+                            parámetro llamado asignatura y valor practica1 y un header llamado
+                            matricula con el valor correspondiente a matrícula asignada. Debe
+                            mostrar la respuesta por la salida estándar.
 
+         */
         Elements form = doc.getElementsByTag("form");
         int cantForm = 0, cantPost = 0, cantGet = 0; // Cantidad de <p> en el HTML
         for(Element frm : form) {
@@ -56,8 +72,24 @@ public class Main {
             {
                 case "post":
                     cantPost++;
+                    /*
+                        Implementacion del Item F
+                     */
+                    URL url1 = new URL(url);
+                    String ulink;
+                    if(url1.getPort() == -1) // Si no tiene puerto
+                    {
+                        ulink = url1.getProtocol()+"://"+url1.getHost()+frm.attr("action");
+                    }else{
+                        ulink = url1.getProtocol()+"://"+url1.getHost()+":"+url1.getPort()+frm.attr("action");
+                    }
+
+                    Document docs = Jsoup.connect(ulink)
+                            .data("asignacion","practica1")
+                            .header("matricula","10138836").post();
+                    System.out.println("\nVariable y header enviado: \n"+docs.html());
                     break;
-                default:
+                default: // Metodo Get y cuando sea default el formulario
                     cantGet++;
                     break;
             }
@@ -65,20 +97,27 @@ public class Main {
             frm.getAllElements().next();
 
         }
-        System.out.println("\n\nInputs");
-        for (Element frm: form){
-            var input = frm.getElementsByTag("input");
-            for(Element f: input)
-            {
-                System.out.println(f);
-                String inputype = input.attr("type");
-                System.out.println("Tipo: " + inputype + "\n");
-            }
-
-        }
-
-        System.out.println("Cantidad de form con el método 'GET' que hay en el HTML: "+cantGet);
+        System.out.println("\nFormularios\nCantidad de form con el método 'GET' que hay en el HTML: "+cantGet);
         System.out.println("Cantidad de form con el método 'POST' que hay en el HTML: "+cantPost);
         System.out.println("Cantidad de form que hay en el HTML: "+cantForm);
+
+        /*
+        Item E: Para cada formulario mostrar los campos del tipo input y su
+                respectivo tipo que contiene en el documento HTML.
+         */
+        System.out.println("\nInputs");
+        for (Element frm: form){
+            var input = frm.getElementsByTag("input");
+            if(input.isEmpty())
+                System.out.println("No tiene inputs.");
+            else{
+                for(Element f: input)
+                {
+                    System.out.println(f);
+                    String inputype = f.attr("type");
+                    System.out.println("Tipo: " + inputype + "\n");
+                }
+            }
+        }
     }
 }
