7d9a5791ad4d87594371fc28711926a9b8b83da8
esuarez <exps0001@ce.pucmm.edu.do>
2022-05-29 21:59:56 -0400

    Creacion de carpetas y archivos

---
+++ ActAula1/src/main/java/Modelo/Usuario.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class Usuario {
+    String usuario;
+    String nombre;
+    String password;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(String usuario, String password)
+    {
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
