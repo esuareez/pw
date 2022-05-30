77730df9ccc11c11828e1fa68b44122f3ea7f452
esuarez <exps0001@ce.pucmm.edu.do>
2022-05-29 22:10:59 -0400

    UsuarioController

--- ActAula1/src/main/java/Controlador/UsuarioController.java
+++ ActAula1/src/main/java/Controlador/UsuarioController.java
@@ -1,5 +1,48 @@
 package Controlador;
 
+import Servicios.fakeServices;
+import io.javalin.Javalin;
+
+
 public class UsuarioController {
+
+    private Javalin app;
+    public UsuarioController(Javalin app)
+    {
+        this.app = app;
+    }
+
+    public void rutas(){
+
+        app.get("/",ctx -> {
+            if(ctx.cookie("usuario") == null){
+                ctx.redirect("formulario.html");
+                return;
+            }
+            ctx.redirect("/inicio");
+            return;
+        });
+
+        app.post("/login", ctx -> {
+           String usuario = ctx.formParam("usuario");
+           String password = ctx.formParam("password");
+           if(usuario == null || password == null)
+           {
+               ctx.redirect("/formulario.html");
+               return;
+           }
+           ctx.cookie("usuario",usuario,180);
+           ctx.redirect("/inicio");
+           return;
+        });
+
+        app.get("/inicio", ctx -> {
+            if(ctx.cookie("usuario") == null){
+                ctx.redirect("formulario.html");
+                return;
+            }
+            ctx.result("Bienvenido de vuelta: "+ctx.cookie("usuario"));
+        });
+    }
 
 }
