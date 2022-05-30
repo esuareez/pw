2cc489faf5e3bb002feac5c9acc1dbd1bd32e86c
esuareez <exps0001@ce.pucmm.edu.do>
2022-05-30 15:26:31 -0400

    Implementacion Thymeleaf

--- ActAula1/src/main/java/Controlador/UsuarioController.java
+++ ActAula1/src/main/java/Controlador/UsuarioController.java
@@ -1,6 +1,10 @@
 package Controlador;
 
+import Modelo.Usuario;
 import io.javalin.Javalin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class UsuarioController {
@@ -13,35 +17,55 @@ public class UsuarioController {
 
     public void rutas(){
 
+        Usuario user = new Usuario("eliam","123");
+
+        app.before(ctx -> {
+            if(ctx.path().startsWith("/formulario.html") || ctx.path().startsWith("/login"))
+            {
+                return;
+            }
+            if(ctx.sessionAttribute("usuario") == null){
+                ctx.redirect("/formulario.html");
+                return;
+            }
+        });
+
         app.get("/",ctx -> {
-            if(ctx.cookie("usuario") == null){
+            if(ctx.sessionAttribute("usuario") == null){
                 ctx.redirect("formulario.html");
                 return;
             }
-            ctx.redirect("/inicio");
-            return;
+            Map<String,Object> modelo = new HashMap<>();
+            modelo.put("usuario",user);
+            ctx.render("/templates/inicio.html",modelo);
         });
 
         app.post("/login", ctx -> {
            String usuario = ctx.formParam("usuario");
            String password = ctx.formParam("password");
-           if(usuario == null || password == null)
-           {
-               ctx.redirect("/formulario.html");
-               return;
-           }
-
-           ctx.cookie("usuario",usuario,180);
-           ctx.redirect("/inicio");
-           return;
+            if((usuario == null || password == null) || !(usuario.equals(user.getUsuario()) && password.equals(user.getPassword())))
+            {
+                ctx.redirect("/formulario.html");
+                return;
+            }
+            if(usuario.equals(user.getUsuario()) && password.equals(user.getPassword()))
+            {
+                ctx.sessionAttribute("usuario",usuario);
+                Map<String,Object> modelo = new HashMap<>();
+                modelo.put("usuario",user);
+                ctx.render("/templates/inicio.html",modelo);
+            }
         });
 
         app.get("/inicio", ctx -> {
-            if(ctx.cookie("usuario") == null){
+            if(ctx.sessionAttribute("usuario") == null){
                 ctx.redirect("formulario.html");
                 return;
             }
-            ctx.redirect("/inicio.html");
+            Map<String,Object> modelo = new HashMap<>();
+            modelo.put("usuario",user);
+            ctx.render("/templates/inicio.html",modelo);
+            //ctx.redirect("/inicio.html");
         });
     }
 
