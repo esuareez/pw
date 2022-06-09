e7bc0650dbfaec7f3e502b769d0d31bcad59d606
esuareez <exps0001@ce.pucmm.edu.do>
2022-06-09 01:42:02 -0400

    ProductoController Creado

--- CRUD-P2/src/main/java/Controlador/ProductoController.java
+++ CRUD-P2/src/main/java/Controlador/ProductoController.java
@@ -1,7 +1,13 @@
 package Controlador;
 
+import Modelos.Producto;
+import Servicios.ProductoServ;
 import Util.BaseController;
 import io.javalin.Javalin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ProductoController extends BaseController {
 
@@ -10,6 +16,65 @@ public class ProductoController extends BaseController {
     }
 
     public void aplicarRutas(){
+        app.get("/",ctx -> {
+            List<Producto> productoList = ProductoServ.getInstance().getProductoList();
+            Map<String,Object> modelo = new HashMap<>();
+            modelo.put("productos",productoList);
+            ctx.render("publico/index.html",modelo);
+        });
 
+        app.get("/crearProducto",ctx -> {
+            ctx.redirect("/crearProducto.html");
+        });
+
+        app.post("/crearProducto", ctx -> {
+            String nombre = ctx.formParam("nombre");
+            int cantidad = Integer.parseInt(ctx.formParam("cantidad"));
+            double precio = Double.parseDouble(ctx.formParam("precio"));
+            String descripcion = ctx.formParam("descripcion");
+            int estado = Integer.parseInt(ctx.formParam("estado"));
+            Producto producto = new Producto(nombre,cantidad,precio,descripcion,estado);
+            ProductoServ.getInstance().crearProducto(producto);
+            ctx.redirect("/");
+        });
+
+        app.get("/inventario",ctx -> {
+            List<Producto> productoList = ProductoServ.getInstance().getProductoList();
+            Map<String,Object> modelo = new HashMap<>();
+            modelo.put("productos",productoList);
+            ctx.render("publico/Inventario.html", modelo);
+        });
+
+        app.get("/producto/editar/{id}",ctx -> {
+            Producto producto = ProductoServ.getInstance().getProductoporID(ctx.pathParamAsClass("id",Integer.class).get());
+            if(producto == null)
+                ctx.redirect("/inventario");
+            Map<String,Object> modelo = new HashMap<>();
+            modelo.put("producto",producto);
+            ctx.render("publico/editarProducto.html",modelo);
+        });
+
+        app.post("/editarProducto",ctx -> {
+            int id = Integer.parseInt(ctx.formParam("id"));
+            String nombre = ctx.formParam("nombre");
+            int cantidad = Integer.parseInt(ctx.formParam("cantidad"));
+            System.out.println(cantidad);
+            double precio = Double.parseDouble(ctx.formParam("precio"));
+            String descripcion = ctx.formParam("descripcion");
+            int estado = Integer.parseInt(ctx.formParam("estado"));
+            Producto producto = new Producto(id,nombre,cantidad,precio,descripcion,estado);
+            ProductoServ.getInstance().editarProducto(producto);
+            Producto p = ProductoServ.getInstance().editarProducto(producto);
+            System.out.println(p.getCantidad());
+            ctx.redirect("/inventario");
+        });
+
+        app.get("/producto/eliminar/{id}",ctx -> {
+            Producto producto = ProductoServ.getInstance().getProductoporID(ctx.pathParamAsClass("id",Integer.class).get());
+            if(producto == null)
+                ctx.redirect("/inventario");
+            ProductoServ.getInstance().deleteProducto(producto);
+            ctx.redirect("/inventario");
+        });
     }
 }
--- CRUD-P2/src/main/java/Main/Main.java
+++ CRUD-P2/src/main/java/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Controlador.ProductoController;
 import Servicios.PedidoServ;
 import Servicios.ProductoServ;
 import Modelos.Producto;
@@ -26,95 +27,7 @@ public class Main {
         });
         app.start(8000);
 
-        app.get("/",ctx -> {
-            List<Producto> productoList = ProductoServ.getInstance().getProductoList();
-            Map<String,Object> modelo = new HashMap<>();
-            modelo.put("productos",productoList);
-            ctx.render("publico/index.html",modelo);
-        });
+        new ProductoController(app).aplicarRutas();
 
-        app.get("/crearProducto",ctx -> {
-            ctx.redirect("/crearProducto.html");
-        });
-
-        app.post("/crearProducto", ctx -> {
-            String nombre = ctx.formParam("nombre");
-            int cantidad = Integer.parseInt(ctx.formParam("cantidad"));
-            double precio = Double.parseDouble(ctx.formParam("precio"));
-            String descripcion = ctx.formParam("descripcion");
-            int estado = Integer.parseInt(ctx.formParam("estado"));
-            Producto producto = new Producto(nombre,cantidad,precio,descripcion,estado);
-            ProductoServ.getInstance().crearProducto(producto);
-            ctx.redirect("/");
-        });
-
-        app.get("/inventario",ctx -> {
-            List<Producto> productoList = ProductoServ.getInstance().getProductoList();
-            Map<String,Object> modelo = new HashMap<>();
-            modelo.put("productos",productoList);
-            ctx.render("publico/Inventario.html", modelo);
-        });
-
-        app.get("/producto/editar/{id}",ctx -> {
-            Producto producto = ProductoServ.getInstance().getProductoporID(ctx.pathParamAsClass("id",Integer.class).get());
-            if(producto == null)
-                ctx.redirect("/inventario");
-            Map<String,Object> modelo = new HashMap<>();
-            modelo.put("producto",producto);
-            ctx.render("publico/editarProducto.html",modelo);
-        });
-
-        app.post("/editarProducto",ctx -> {
-            int id = Integer.parseInt(ctx.formParam("id"));
-            String nombre = ctx.formParam("nombre");
-            int cantidad = Integer.parseInt(ctx.formParam("cantidad"));
-            System.out.println(cantidad);
-            double precio = Double.parseDouble(ctx.formParam("precio"));
-            String descripcion = ctx.formParam("descripcion");
-            int estado = Integer.parseInt(ctx.formParam("estado"));
-            Producto producto = new Producto(id,nombre,cantidad,precio,descripcion,estado);
-            ProductoServ.getInstance().editarProducto(producto);
-            Producto p = ProductoServ.getInstance().editarProducto(producto);
-            System.out.println(p.getCantidad());
-            ctx.redirect("/inventario");
-        });
-
-        app.get("/producto/eliminar/{id}",ctx -> {
-            Producto producto = ProductoServ.getInstance().getProductoporID(ctx.pathParamAsClass("id",Integer.class).get());
-            if(producto == null)
-                ctx.redirect("/inventario");
-            ProductoServ.getInstance().deleteProducto(producto);
-            ctx.redirect("/inventario");
-        });
-
-        /*System.out.println("Productos registrados.");
-        for(var item :ProductoServ.getInstance().getProductoList())
-        {
-            System.out.println(item.getNombre()+" "+item.getPrecio()+" "+item.getEstado()+" "+item.getDescripcion());
-        }
-        System.out.println("\nProductos luego de editar.");
-        Producto producto = new Producto(2,"Winasorb Ultra",50,35,"Dolor del cuerpo.",0);
-        ProductoServ.getInstance().editarProducto(producto);
-        for(var item :ProductoServ.getInstance().getProductoList())
-        {
-            System.out.println(item.getNombre()+" "+item.getPrecio()+" "+item.getEstado()+" "+item.getDescripcion());
-        }
-        System.out.println("\nProductos actuales.");
-        ProductoServ.getInstance().deleteProducto(producto);
-        for(var item :ProductoServ.getInstance().getProductoList())
-        {
-            if(item.getEstado()>0)
-                System.out.println(item.getNombre()+" "+item.getPrecio()+" "+item.getEstado()+" "+item.getDescripcion());
-        }
-        System.out.println("\nProductos en el pedido");
-        for(var item: PedidoServ.getInstance().getPedidoList()){
-            System.out.println(item.getId());
-            for(var product : item.getProductoPedido())
-            {
-                System.out.println("Producto: "+product.getProducto().getNombre()+" Cantidad en el pedido:"+
-                        product.getCantidad()+" Id del pedido:"+product.getPedido().getId());
-            }
-            System.out.println(item.getFecha()+"\n"+item.getNombre());
-        }*/
     }
 }
